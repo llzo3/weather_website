6aa659050efbf1e29542a21b1fb0f37f12f4305f
llzo3 <ryeod03@gmail.com>
2024-05-17 15:28:28 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,9 +4,12 @@ import requests
 
 app = FastAPI()
 
+# const lat = 37.477550020716194
+# const lon = 126.98212524649105
+
 @app.get("/")
 def root():
-    URL = "https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&units=metric&appId=2c1d56123e5c377524128dbc99536f60"
+    URL = "https://api.openweathermap.org/data/2.5/onecall?lat=$37.477550020716194&lon=$126.98212524649105&exclude=minutely,hourly&units=metric&appId=2c1d56123e5c377524128dbc99536f60"
     contents = requests.get(URL).text
     
     return { "message": contents }
