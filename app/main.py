c8f2b932a55bbf607c183bcd25febd8f0bc41b7b
llzo3 <ryeod03@gmail.com>
2024-05-17 15:55:03 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,18 +1,24 @@
 from fastapi import FastAPI
-
 import requests
 
 app = FastAPI()
 
-# const lat = 37.477550020716194
-# const lon = 126.98212524649105
+LAT = 37.477550020716194
+LON = 126.98212524649105
+API_KEY = "2c1d56123e5c377524128dbc99536f60"
 
 @app.get("/")
 def root():
-    URL = "https://api.openweathermap.org/data/2.5/onecall?lat=$37.477550020716194&lon=$126.98212524649105&exclude=minutely,hourly&units=metric&appId=2c1d56123e5c377524128dbc99536f60"
-    contents = requests.get(URL).text
+    URL = f"https://api.openweathermap.org/data/2.5/onecall?lat={LAT}&lon={LON}&exclude=minutely,hourly&units=metric&appid={API_KEY}"
+    response = requests.get(URL)
     
-    return { "message": contents }
+    # 응답이 성공적인지 확인 (HTTP 상태 코드 200)
+    if response.status_code == 200:
+        contents = response.json()
+    else:
+        contents = response.text  # 에러 메시지를 그대로 반환
+
+    return {"message": contents}
 
 @app.get("/home")
 def home():
