3ec350c53d525cae36867701fa7cc8db0ea8a6be
llzo3 <ryeod03@gmail.com>
2024-05-17 16:51:33 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI
 import requests
 
+
+city = "Seoul"
 app = FastAPI()
 
 LAT = 37.477550020716194
 LON = 126.98212524649105
 API_KEY = "7984a6ee79bc96d84c6a09aaf4cdf934"
 
+
 @app.get("/")
 def root():
-    URL = f"http://api.openweathermap.org/data/2.5/forecast?lat={LAT}&lon={LON}&appid={API_KEY}"
+    URL = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metirc&lang=kr"
     response = requests.get(URL)
 
     return {"message": response}
