ae31ed47e6a517d07968a448b986ed96396ebc49
llzo3 <ryeod03@gmail.com>
2024-05-17 16:25:34 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -11,14 +11,8 @@ API_KEY = "7984a6ee79bc96d84c6a09aaf4cdf934"
 def root():
     URL = f"http://api.openweathermap.org/data/2.5/forecast?lat={LAT}&lon={LON}&appid={API_KEY}"
     response = requests.get(URL)
-    
-    # 응답이 성공적인지 확인 (HTTP 상태 코드 200)
-    if response.status_code == 200:
-        contents = response.json()
-    else:
-        contents = response.text  # 에러 메시지를 그대로 반환
 
-    return {"message": contents}
+    return {"message": response}
 
 @app.get("/home")
 def home():
