1f17d7efd5fb0764cfd288f5ee52c881e2fc5748
llzo3 <ryeod03@gmail.com>
2024-05-17 16:55:08 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 import requests
 
-
 city = "Seoul"
 app = FastAPI()
 
@@ -9,13 +8,16 @@ LAT = 37.477550020716194
 LON = 126.98212524649105
 API_KEY = "7984a6ee79bc96d84c6a09aaf4cdf934"
 
-
 @app.get("/")
 def root():
-    URL = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metirc&lang=kr"
+    URL = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric&lang=kr"
     response = requests.get(URL)
 
-    return {"message": response}
+    if response.status_code == 200:
+        data = response.json()
+        return {"message": data}
+    else:
+        return {"message": "Failed to fetch weather data"}
 
 @app.get("/home")
 def home():
