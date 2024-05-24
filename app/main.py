95ba8f98c5ddcae04eb5028026a55894ef31d1a6
llzo3 <ryeod03@gmail.com>
2024-05-24 15:50:19 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import requests
 
 city = "Seoul"
@@ -8,14 +10,51 @@ LAT = 37.477550020716194
 LON = 126.98212524649105
 API_KEY = "7984a6ee79bc96d84c6a09aaf4cdf934"
 
-@app.get("/")
-def root():
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", , response_class=HTMLResponse)
+def root(request: Request):
     URL = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric&lang=kr"
     response = requests.get(URL)
 
     if response.status_code == 200:
         data = response.json()
-        return {"message": data}
+        formatted_data = {
+            "location": {
+                "city": data["name"],
+                "country": data["sys"]["country"],
+                "coordinates": {
+                    "latitude": data["coord"]["lat"],
+                    "longitude": data["coord"]["lon"]
+                }
+            },
+            "weather": {
+                "description": data["weather"][0]["description"],
+                "temperature": {
+                    "current": data["main"]["temp"],
+                    "feels_like": data["main"]["feels_like"],
+                    "min": data["main"]["temp_min"],
+                    "max": data["main"]["temp_max"]
+                },
+                "humidity": data["main"]["humidity"],
+                "pressure": data["main"]["pressure"],
+                "visibility": data["visibility"]
+            },
+            "wind": {
+                "speed": data["wind"]["speed"],
+                "direction": data["wind"]["deg"]
+            },
+            "clouds": {
+                "coverage": data["clouds"]["all"]
+            },
+            "sun": {
+                "sunrise": data["sys"]["sunrise"],
+                "sunset": data["sys"]["sunset"]
+            },
+            "timezone": data["timezone"],
+            "timestamp": data["dt"]
+        }
+        return templates.TemplateResponse("weather.html", {"request": request, "data": formatted_data})
     else:
         return {"message": "Failed to fetch weather data"}
 
